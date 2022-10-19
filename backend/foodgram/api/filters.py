97b9ae0477ae474b653bde65b792d85aa7861819
Антон Борисов <ivan-kruzenstern@yandex.ru>
2022-10-19 12:44:57 +0300

    fixed recipes filtering

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -11,8 +11,7 @@ class RecipeFilter(filters.FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
-        queryset=Tag.objects.all(),
-        method='filter_tags'
+        queryset=Tag.objects.all()
     )
 
     class Meta:
@@ -21,19 +20,6 @@ class RecipeFilter(filters.FilterSet):
             'author',
             'tags'
         )
-
-    def filter_tags(self, queryset, name, value):
-        if not value:
-            return queryset
-        exclude_tags = Tag.objects.exclude(
-            slug__in=[tag.slug for tag in value]
-        )
-        queryset = queryset.filter(
-            tags__in=value
-        ).exclude(
-            tags__in=exclude_tags
-        ).distinct()
-        return queryset
 
 
 class IngredientFilter(filters.FilterSet):
